cbc8618d314f75ff582ef360ecd1a1be620eaf38
MonsterV82152 <eddyliang193@gmail.com>
2024-07-18 11:10:28 -0400

    bob

--- Bot #1/src/main.cpp
+++ Bot #1/src/main.cpp
@@ -77,74 +77,106 @@ void autonomous() {}
 void opcontrol() {
 	pros::ADIDigitalOut clawp ('A');
 	pros::Motor intake(-5);
-	pros::Motor fourbar(17);
+	pros::Motor hook(-17);
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	pros::MotorGroup left_mg({-1, -2, -3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
 	pros::MotorGroup right_mg({10, 6, 7});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 	intake.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
 	left_mg.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
 	right_mg.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-	int intakecooldown = 25;
-	int clawcooldown = 25;
+
 	bool boolintake = false;
-	bool claw = false;
+	bool boolhook = false;
+
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
-
-
-
-		if (intakecooldown != 0) {
-			intakecooldown = intakecooldown-1;
-			//pros::lcd::set_text(1, intakecooldown);
-			}
-		if (clawcooldown != 0) {
-			clawcooldown = clawcooldown-1;
+		int dir = master.get_analog(ANALOG_LEFT_Y);
+		int turn = master.get_analog(ANALOG_RIGHT_Y);
+		left_mg.move(dir);
+		right_mg.move(turn);
+		if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1)) {
+			
+			if (boolintake) {
+				intake.move(127);
+			} else {
+				intake.brake();
 			}
 
-		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_X) == true and intakecooldown == 0) {
 			boolintake = !boolintake;
-			intakecooldown = 25;
+
 		}
-		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_A) == true and clawcooldown == 0) {
-			bool claw = !claw;
-			if (claw) {
-				clawp.set_value(true);
+				
+		if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_X)) {
+			if (boolhook) {
+				hook.move(127);
+			} else {
+				hook.brake();
 			}
-			else {
-				clawp.set_value(false);
-			}
-			clawcooldown = 25;
+			
+			boolhook = !boolhook;
+
 		}
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_Y);  // Gets the turn left/right from right joystick
-		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_Y)) {
-			intake.move(-127);
-		}
-		else if (boolintake) {
-			intake.move(127);
-		}
-		else {
-			intake.brake();
-		}
-		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_UP)) {
-			fourbar.move(127);
-		}
-		else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)) {
-			fourbar.move(-127);
-		}
-		else {
-			fourbar.brake();
-		}
-		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT)) {
-			fourbar.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-		}
-		else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT)) {
-			fourbar.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-		}
-		left_mg.move(dir);                      // Sets left motor voltage
-		right_mg.move(turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
+
+	pros::delay(20);  
 	}
+	// int intakecooldown = 25;
+	// int clawcooldown = 25;
+	// bool boolintake = false;
+	// bool claw = false;
+	// while (true) {
+	// 	pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
+	// 	                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
+	// 	                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
+
+
+
+	// 	if (intakecooldown != 0) {
+	// 		intakecooldown = intakecooldown-1;
+	// 		//pros::lcd::set_text(1, intakecooldown);
+	// 		}
+	// 	if (clawcooldown != 0) {
+	// 		clawcooldown = clawcooldown-1;
+	// 		}
+
+	// 	if (master.get_digital(pros::E_CONTROLLER_DIGITAL_X) == true and intakecooldown == 0) {
+	// 		boolintake = !boolintake;
+	// 		intakecooldown = 25;
+	// 	}
+	// 	if (master.get_digital(pros::E_CONTROLLER_DIGITAL_A) == true and clawcooldown == 0) {
+	// 		bool claw = !claw;
+	// 		if (claw) {
+	// 			clawp.set_value(true);
+	// 		}
+	// 		else {
+	// 			clawp.set_value(false);
+	// 		}
+	// 		clawcooldown = 25;
+	// 	}
+		  // Gets the turn left/right from right joystick
+	// 	if (master.get_digital(pros::E_CONTROLLER_DIGITAL_Y)) {
+	// 		intake.move(-127);
+	// 	}
+	// 	else if (boolintake) {
+	// 		intake.move(127);
+	// 	}
+	// 	else {
+	// 		intake.brake();
+	// 	}
+	// 	if (master.get_digital(pros::E_CONTROLLER_DIGITAL_UP)) {
+	// 		fourbar.move(127);
+	// 	}
+	// 	else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)) {
+	// 		fourbar.move(-127);
+	// 	}
+	// 	else {
+	// 		fourbar.brake();
+	// 	}
+	// 	if (master.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT)) {
+	// 		fourbar.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+	// 	}
+	// 	else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT)) {
+	// 		fourbar.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+	// 	}
+		                  // Sets right motor voltage
+	// 	                             // Run for 20 ms then update
+	// }
 }
